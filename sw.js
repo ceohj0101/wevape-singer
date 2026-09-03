a85b220b94b1f03b30861500b1b50a9358c9bd16
ceohj0101 <ceohj01@gmail.com>
2026-09-03 12:36:03 +0900

    디제이 부스 추가(2덱·크로스페이더·런치패드 16패드) · 차영근 「다음에 또 만나요」 출시 · sw v13

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "wevape-singer-v11";
+const CACHE = "wevape-singer-v13";
 const CORE = ["./manifest.json", "./icon.png", "./icon-192.png"];
 
 self.addEventListener("install", (e) => {
